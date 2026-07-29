7c70c619ca116b809e686b662ee8a744fc20f7cf
LI WEIRAN <vranlee86@gmail.com>
2026-07-29 14:30:17 +0800

    INIT.

--- src/lib/models/tidel.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from .yolo import get_pose_net as _get_pose_net
-
-
-def get_pose_net(*args, **kwargs):
-    return _get_pose_net(*args, **kwargs)
